3fe80ec2cca2d79899f873598d0b406ebd1a6d42
Jiwook <38754176+b1ackapple@users.noreply.github.com>
2018-05-21 17:03:10 +0000

    Update 소스.cpp

--- 소스.cpp
+++ 소스.cpp
@@ -37,21 +37,3 @@ public:
 	void setArr(string& org) { org += input; }
 
 };
-int main(void)
-{
-	string Arr;
-	start s(Arr);
-	s.openSet(Arr);
-	body b(Arr);
-	b.openSet(Arr);
-	h1 h(Arr);
-	h.openSet(Arr);
-	userinput u;
-	u.setArr(Arr);
-	h.closeSet(Arr);
-	b.closeSet(Arr);
-	s.closeSet(Arr);
-
-	cout << Arr;
-
-}
