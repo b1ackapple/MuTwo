7ab96035aec4a88fbbe62b51b94a533de8bbcc62
Jiwook <38754176+b1ackapple@users.noreply.github.com>
2018-05-22 02:08:18 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main(void)
 {
+	string Arr;
+	start s(Arr);
+	s.openSet(Arr);
+	body b(Arr);
+	b.openSet(Arr);
+	h1 h(Arr);
+	h.openSet(Arr);
+	userinput u;
+	u.setArr(Arr);
+	h.closeSet(Arr);
+	b.closeSet(Arr);
+	s.closeSet(Arr);
+	cout << Arr;
 	return 0;
 }
