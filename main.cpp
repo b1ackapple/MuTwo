ddaed235a86e6d9defd25787f51e3e0633f25da4
Jiwook <38754176+b1ackapple@users.noreply.github.com>
2018-05-27 08:59:50 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
-#include "source.h"
+#include header.h"
 
 using namespace std;
 
 int main(void)
 {
 	string Arr;
-	start s(Arr);
+	
+	start s(Arr);//start 클래스 s 객체 생성
 	s.openSet(Arr);
-	body b(Arr);
+	
+	body b(Arr);//body 클래스 b 객체 생성
 	b.openSet(Arr);
-	h1 h(Arr);
+	
+	h1 h(Arr);//h1 클래스 h 객체 생성
 	h.openSet(Arr);
-	userinput u;
+	
+	userinput u;//userinput 클래스 u 객체 생성
 	u.setArr(Arr);
+	
 	h.closeSet(Arr);
 	b.closeSet(Arr);
 	s.closeSet(Arr);
