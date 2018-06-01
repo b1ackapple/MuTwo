e077c259bb2d833ed421656e069d72cf092df141
Jiwook <38754176+b1ackapple@users.noreply.github.com>
2018-06-01 14:48:04 +0900

    Update Font.h

--- Mutwo/Mutwo/Font.h
+++ Mutwo/Mutwo/Font.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 class hnumber {
@@ -15,3 +16,12 @@ public:
 	hnumber();
 	void openSet(string &org) { org += open; }
 	void closeSet(string &org) { org += close; }
+}
+
+hnumber::hnumber()
+{
+	cout << "숫자 입력 (1~6) : ";
+	getline(cin, input);
+	open = "<h" + input + ">\t";
+	close = "\t</h" + input + ">\n";
+}
