d3a14f9bb634e3a70078d727ec04c2da5d1bfa48
MINI <koreajongmin72024@gmail.com>
2018-06-03 12:44:37 +0900

    All bug fix.
    I fix no .html in the file name.
    Fix bugs where project files do not run.
    the questions in the program are natural and accurate.
    
    MINI on 2018-06-03-12:43(KST).

---
+++ Mutwo/Mutwo/Font.h
@@ -0,0 +1,21 @@
+#pragma once
+
+using namespace std;
+class h_number {
+	string open;
+	string input;
+	string close;
+public:
+	h_number(string &org);
+	void closeSet(string &org) { org += close; }
+};
+
+h_number::h_number(string &org)
+{
+	cout << "글자크기를 입력하세요 (1~6) : ";
+	getline(cin, input);
+	open = "\t<h" + input + ">\t";
+	close = "\t</h" + input + ">\n";
+	org += open;
+}
+
