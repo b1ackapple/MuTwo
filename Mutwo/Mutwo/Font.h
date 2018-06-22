3c968293ee37914103ba0651925d798a7e5f4abb
kwjinwoo <38836328+kwjinwoo@users.noreply.github.com>
2018-06-22 14:06:55 +0900

    주석
    made by jiwook

--- Mutwo/Mutwo/Font.h
+++ Mutwo/Mutwo/Font.h
@@ -5,18 +5,18 @@ class h_number {
 	string input;
 	string close;
 public:
-	void openSet(string &org) { org += open; }
+	void openSet(string &org) { org += open; }//코드 입력
 	void closeSet(string &org) { org += close; }
 	void Setfont();
 };
 
-void h_number::Setfont()
+void h_number::Setfont()//폰트 설정 함수
 {
 	cout << "select font(1~6) : ";
-	cin.ignore();
-	getline(cin, input);
+	cin.ignore();//버퍼 비우기
+	getline(cin, input);//입력
 	system("Cls");
-	open = "\t<h" + input + ">\t";
-	close = "\t</h" + input + ">\n";
+	open = "\t<h" + input + ">\t";//태그 열기
+	close = "\t</h" + input + ">\n";//태그 닫기
 	
 }
